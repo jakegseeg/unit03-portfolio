f9634d0cb7b656eef9d313d9b518712e21c5cae7
Jake Seegmiller <jakegseeg@gmail.com>
2025-12-08 23:25:27 -0700

    doubled down

--- startHere/js/scripts.js
+++ startHere/js/scripts.js
@@ -2,4 +2,4 @@ let x = new Date();
 console.log(x);
 let y = x.getFullYear();
 console.log(y);
-document.querySelector('#theYear').textContent = y;
+document.querySelector("#theYear").textContent = y;
